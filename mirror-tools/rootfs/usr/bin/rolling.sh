ede42afe0924f075728415c13fda70d7497d1235
Pascal Bellard <pascal.bellard@slitaz.org>
2012-02-24 13:12:07 +0100

    rolling.sh: fix group owner

--- mirror-tools/rootfs/usr/bin/rolling.sh
+++ mirror-tools/rootfs/usr/bin/rolling.sh
@@ -14,6 +14,7 @@ htmlize()
 echo -e "<html>\n<body>\n<pre>"
 dos2unix | sed -e 's|\(Filesystem size:\).*G\([0-9\.]*M\) *$|\1 \2|' \
     -e 's|.\[1m|<b>|' -e 's|.\[0m|</b>|' -e 's|.\[[0-9Gm;]*||g' \
+    -e 's|#.*|<i><span style="color: blue">&</span></i>|' \
     -e ':a;s/^\(.\{1,68\}\)\(\[ [A-Za-z]* \]\)/\1 \2/;ta' \
     -e 's#\[ OK \]#[ <span style="color: green">OK</span> ]#' \
     -e 's#\[ Failed \]#[ <span style="color: red">Failed</span> ]#'
@@ -29,16 +30,18 @@ for flavor in $flavors ; do
 	TMP=$rolling/tmp$$
 	mkdir -p $TMP/iso $TMP/fs/var/lib/tazpkg $TMP/fs/home/slitaz/cooking \
 		 $TMP/fs/var/cache/tazpkg/cooking/packages
+	chown -R root.root $TMP
+	chmod -R 755 $TMP
+	mount -o loop,ro $system $TMP/iso
+	for i in $(ls -r $TMP/iso/boot/rootfs*.gz) ; do
+		unlzma -c $i | ( cd $TMP/fs ; cpio -idmu )
+	done
 	mount --bind $packages $TMP/fs/var/cache/tazpkg/cooking/packages
 	ln -s /var/cache/tazpkg/cooking/packages $TMP/fs/home/slitaz/cooking
 	# 3.0 compatibility...
 	ln -s cooking/packages $TMP/fs/home/slitaz/packages
 	cp -a $packages/packages.* $TMP/fs/var/lib/tazpkg
 	cp $packages/$flavor.flavor $TMP/fs
-	mount -o loop,ro $system $TMP/iso
-	for i in $TMP/iso/boot/rootfs*.gz ; do
-		unlzma -c $i | ( cd $TMP/fs ; cpio -id )
-	done
 	[ -d $rolling/fixes ] && cp -a $rolling/fixes/. $TMP/fs/.
 	echo "cooking" > $TMP/fs/etc/slitaz-release
 	umount -d $TMP/iso
@@ -83,7 +86,9 @@ EOT
 	rm -rf $TMP
     fi
     export DROPBEAR_PASSWORD=none
-    SSH="dbclient -p 222 -i /home/bellard/.ssh/id_rsa.dropbear"
-    rsync --bwlimit=40 -vtP -e "$SSH" $rolling/slitaz-$flavor.* \
+    SSH="dbclient -i /home/bellard/.ssh/id_rsa.dropbear"
+    #BWLIMIT="--bwlimit=40"
+    BWLIMIT=""
+    rsync $BWLIMIT -vtP -e "$SSH" $rolling/slitaz-$flavor.* \
 	bellard@mirror.slitaz.org:/var/www/slitaz/mirror/iso/rolling
 done
